3af4e919ad116767b22908d054743e22575a535b
arans <ayooi88@gmail.com>
2021-06-03 14:05:32 +1000

    Ok this seems to be a pretty good example of a test app that registers itself repeatedly

--- Hello.cpp
+++ Hello.cpp
@@ -3,8 +3,62 @@
 //
 #include <iostream>
 #include <zmq.h>
+#include <thread>
 
-int main(int argc, char **argv) {
+using namespace std::chrono_literals;
+
+class DataServiceWriter {
+public:
+
+    DataServiceWriter(const std::string &serviceName,
+                      const std::string &dataUrl,
+                      void *ctx,
+                      const std::string &serviceLocatorUrl)
+            : _running(true),
+              _serviceName(serviceName),
+              _dataUrl(dataUrl) {
+        _dataSocket = zmq_socket(ctx, ZMQ_PUSH);
+        long val = 1000;
+        zmq_setsockopt(_dataSocket, ZMQ_SNDHWM, &val, sizeof(long));
+        zmq_bind(_dataSocket, dataUrl.c_str());
+
+        _serviceSocket = zmq_socket(ctx, ZMQ_DEALER);
+        zmq_setsockopt(_serviceSocket, ZMQ_IDENTITY, &dataUrl, dataUrl.length());
+        zmq_connect(_serviceSocket, serviceLocatorUrl.c_str());
+
+        _thread = std::thread([&]() {
+            while (_running) {
+                std::string command = "register";
+                zmq_send(_serviceSocket, command.c_str(), command.length(), ZMQ_SNDMORE);
+                zmq_send(_serviceSocket, _serviceName.c_str(), _serviceName.length(), ZMQ_SNDMORE);
+                zmq_send(_serviceSocket, _dataUrl.c_str(), _dataUrl.length(), 0);
+                std::this_thread::sleep_for(5s);
+            }
+        });
+    }
+
+    ~DataServiceWriter() {
+        _running = false;
+        zmq_close(_serviceSocket);
+        zmq_close(_dataSocket);
+        _thread.join();
+    }
+
+    void write(const uint8_t *payload, const size_t length) {
+        zmq_send(_dataSocket, payload, length, 0);
+    }
+
+private:
+    void *_dataSocket;
+    void *_serviceSocket;
+
+    std::thread _thread;
+    bool _running;
+    const std::string _dataUrl;
+    const std::string _serviceName;
+};
+
+void simple_test() {
     std::cout << "Hello World!" << std::endl;
     auto ctx = zmq_ctx_new();
     auto push = zmq_socket(ctx, ZMQ_PUSH);
@@ -21,11 +75,27 @@ int main(int argc, char **argv) {
         zmq_msg_t msg;
         zmq_msg_init(&msg);
         zmq_recvmsg(pull, &msg, 0);
-        std::cout << std::string(static_cast<char*>(zmq_msg_data(&msg)), zmq_msg_size(&msg)) << std::endl;
+        std::cout << std::string(static_cast<char *>(zmq_msg_data(&msg)), zmq_msg_size(&msg)) << std::endl;
     }
 
     zmq_close(push);
     zmq_close(pull);
     zmq_ctx_destroy(ctx);
+}
+
+int main(int argc, char **argv) {
+//    simple_test();
+
+    auto ctx = zmq_ctx_new();
+    std::string serviceName = "service-name";
+    std::string dataUrl = "tcp://localhost:15000";
+    std::string serviceLocatorUrl = "tcp://localhost:19999";
+
+    DataServiceWriter writer(serviceName, dataUrl, ctx, serviceLocatorUrl);
+
+    while (true) {
+        std::this_thread::sleep_for(5s);
+    }
+
     return 0;
 }
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # ZeroMQ Data Streaming Client
 
-For use in conjunction with https://github.com/ayooi/zmq-data-streaming 
+For use in conjunction with https://github.com/ayooi/zmq-data-streaming
+
+### Misc
+
+This project uses Java style coding conventions. Yes its annoying but it matches our code base so i'm going to stick to
+it 
