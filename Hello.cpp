cbb1a6daef5470805a3d96309a28165a260f611a
arans <ayooi88@gmail.com>
2021-06-03 16:28:36 +1000

    Continuing working on the reader

--- Hello.cpp
+++ Hello.cpp
@@ -6,16 +6,18 @@
 #include <thread>
 #include <list>
 #include <set>
+#include <utility>
+#include <vector>
 
 using namespace std::chrono_literals;
 
 class DataServiceReader {
 public:
     DataServiceReader(const std::string &uniqueName,
-                      const std::string &serviceName,
+                      std::string serviceName,
                       void *ctx,
                       const std::string &serviceLocatorUrl)
-            : _serviceName(serviceName),
+            : _serviceName(std::move(serviceName)),
               _running(true) {
         _locatorSocket = zmq_socket(ctx, ZMQ_DEALER);
         zmq_setsockopt(_locatorSocket, ZMQ_IDENTITY, &uniqueName, uniqueName.length());
@@ -30,17 +32,86 @@ public:
         _dataSocket = zmq_socket(ctx, ZMQ_PULL);
         zmq_connect(_dataSocket, internalControlUrl.c_str());
 
-        _thread = std::thread([&]() {
+        _queryThread = std::thread([&]() {
+            zmq_pollitem_t pollitems[] = {
+                    {_locatorSocket, 0, ZMQ_POLLIN, 0}
+            };
+            std::string command = "query";
             while (_running) {
+                zmq_send(_locatorSocket, command.c_str(), command.length(), ZMQ_SNDMORE);
+                zmq_send(_locatorSocket, _serviceName.c_str(), _serviceName.length(), 0);
 
+                int rc = zmq_poll(pollitems, 1, 10000);
+                if (rc == -1) {
+                    break;
+                }
+                std::set<std::string> locations;
+                for (; pollitems[0].revents & ZMQ_POLLIN;
+                       zmq_poll(pollitems, 1, 0)) {
+                    zmq_msg_t msgs;
+                    zmq_msg_init(&msgs);
+                    zmq_msg_recv(&msgs, _locatorSocket, 0);
+                    auto location = std::string(static_cast<char *>(zmq_msg_data(&msgs)), zmq_msg_size(&msgs));
+                    locations.insert(location);
+                    zmq_msg_close(&msgs);
+                }
+                doConnects(locations);
+            }
+        });
+
+        _processingThread = std::thread([&]() {
+            while (_running) {
+                zmq_msg_t msg;
+                zmq_msg_init(&msg);
+                zmq_msg_recv(&msg, _dataSocket, 0);
+
+                size_t size = zmq_msg_size(&msg);
+                auto dataPtr = static_cast<uint8_t *>(zmq_msg_data(&msg));
+                std::vector<uint8_t> payload(dataPtr, dataPtr + size);
+                _payloads.push_back(payload);
+
+                zmq_msg_close(&msg);
             }
         });
     }
 
+    void doConnects(const std::set<std::string> &incoming) {
+        std::list<std::string> toConnect;
+        for (auto const &l: incoming) {
+            if (_locations.count(l) == 0) {
+                toConnect.push_back(l);
+            }
+        }
+
+        std::list<std::string> toDisconnect;
+        for (const auto &l : _locations) {
+            if (incoming.count(l) == 0) {
+                toDisconnect.push_back(l);
+            }
+        }
+
+        if (!toDisconnect.empty() || !toConnect.empty()) {
+            for (const auto &disconnect : toDisconnect) {
+                zmq_disconnect(_dataSocket, disconnect.c_str());
+                std::cout << "[Reader] Disconnecting from old provider " << disconnect << std::endl;
+            }
+
+            for (const auto &connect : toConnect) {
+                zmq_connect(_dataSocket, connect.c_str());
+                std::cout << "[Reader] Connecting to new provider " << connect << std::endl;
+            }
+            std::string ignoreMe = "###TheIgnoreMeMessage###";
+            zmq_send(_controlSocket, ignoreMe.c_str(), ignoreMe.length(), 0);
+        }
+    }
+
     ~DataServiceReader() {
+        _running = false;
         zmq_close(_controlSocket);
         zmq_close(_dataSocket);
         zmq_close(_locatorSocket);
+        _queryThread.join();
+        _processingThread.join();
     }
 
 private:
@@ -49,10 +120,11 @@ private:
     void *_controlSocket;
 
     const std::string _serviceName;
-    std::thread _thread;
+    std::thread _queryThread;
+    std::thread _processingThread;
     bool _running;
 
-    std::list<uint8_t[]> _payloads;
+    std::list<std::vector<uint8_t>> _payloads;
     std::set<std::string> _locations;
 
 };
@@ -149,9 +221,15 @@ int main(int argc, char **argv) {
 
     DataServiceWriter writer(serviceName, dataUrl, ctx, serviceLocatorUrl);
 
+    DataServiceReader reader("random-name",
+                             serviceName,
+                             ctx,
+                             serviceLocatorUrl);
+
     while (true) {
         std::this_thread::sleep_for(5s);
     }
+
 
     return 0;
 }
